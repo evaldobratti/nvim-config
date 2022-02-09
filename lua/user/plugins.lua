8e5f9cddeeb9297f72b3ad0286578be5f84a7cdb
Evaldo Bratti <evaldo.bratti@alphasights.com>
2022-02-09 08:51:29 -0300

    gitsigns

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -77,6 +77,8 @@ return packer.startup(function(use)
   use "akinsho/bufferline.nvim"
   use "moll/vim-bbye"
 
+  use "lewis6991/gitsigns.nvim"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
