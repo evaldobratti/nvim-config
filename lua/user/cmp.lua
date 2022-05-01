baa10a4ac4a09ad1a47eb76a485fb25f819640bb
Evaldo Bratti <evaldo.bratti@gmail.com>
2022-05-01 15:16:27 -0300

    fix plugins to latest versions

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -79,10 +79,10 @@ cmp.setup {
     behavior = cmp.ConfirmBehavior.Replace,
     select = false,
   },
-  documentation = false,
-  --documentation = {
-  --  border = { "?", "?", "?", "?", "?", "?", "?", "?" },
-  --},
+  window = {
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+  },
   experimental = {
     ghost_text = false,
     native_menu = false,
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -44,8 +44,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/Users/user/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/Users/user/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/Users/user/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/Users/user/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/Users/user/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
+local package_path_str = "/home/evaldo/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/home/evaldo/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/home/evaldo/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/home/evaldo/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/home/evaldo/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -71,122 +71,122 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   LuaSnip = {
     loaded = true,
-    path = "/Users/user/.local/share/nvim/site/pack/packer/start/LuaSnip",
+    path = "/home/evaldo/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
   ["bufferline.nvim"] = {
     loaded = true,
-    path = "/Users/user/.local/share/nvim/site/pack/packer/start/bufferline.nvim",
+    path = "/home/evaldo/.local/share/nvim/site/pack/packer/start/bufferline.nvim",
     url = "https://github.com/akinsho/bufferline.nvim"
   },
   ["cmp-buffer"] = {
     loaded = true,
-    path = "/Users/user/.local/share/nvim/site/pack/packer/start/cmp-buffer",
+    path = "/home/evaldo/.local/share/nvim/site/pack/packer/start/cmp-buffer",
     url = "https://github.com/hrsh7th/cmp-buffer"
   },
   ["cmp-cmdline"] = {
     loaded = true,
-    path = "/Users/user/.local/share/nvim/site/pack/packer/start/cmp-cmdline",
+    path = "/home/evaldo/.local/share/nvim/site/pack/packer/start/cmp-cmdline",
     url = "https://github.com/hrsh7th/cmp-cmdline"
   },
   ["cmp-nvim-lsp"] = {
     loaded = true,
-    path = "/Users/user/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
+    path = "/home/evaldo/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
   ["cmp-path"] = {
     loaded = true,
-    path = "/Users/user/.local/share/nvim/site/pack/packer/start/cmp-path",
+    path = "/home/evaldo/.local/share/nvim/site/pack/packer/start/cmp-path",
     url = "https://github.com/hrsh7th/cmp-path"
   },
   colorschemes = {
     loaded = true,
-    path = "/Users/user/.local/share/nvim/site/pack/packer/start/colorschemes",
+    path = "/home/evaldo/.local/share/nvim/site/pack/packer/start/colorschemes",
     url = "https://github.com/lunarvim/colorschemes"
   },
   ["friendly-snippets"] = {
     loaded = true,
-    path = "/Users/user/.local/share/nvim/site/pack/packer/start/friendly-snippets",
+    path = "/home/evaldo/.local/share/nvim/site/pack/packer/start/friendly-snippets",
     url = "https://github.com/rafamadriz/friendly-snippets"
   },
   ["gitsigns.nvim"] = {
     loaded = true,
-    path = "/Users/user/.local/share/nvim/site/pack/packer/start/gitsigns.nvim",
+    path = "/home/evaldo/.local/share/nvim/site/pack/packer/start/gitsigns.nvim",
     url = "https://github.com/lewis6991/gitsigns.nvim"
   },
   ["nvim-cmp"] = {
     loaded = true,
-    path = "/Users/user/.local/share/nvim/site/pack/packer/start/nvim-cmp",
+    path = "/home/evaldo/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
   ["nvim-lsp-installer"] = {
     loaded = true,
-    path = "/Users/user/.local/share/nvim/site/pack/packer/start/nvim-lsp-installer",
+    path = "/home/evaldo/.local/share/nvim/site/pack/packer/start/nvim-lsp-installer",
     url = "https://github.com/williamboman/nvim-lsp-installer"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
-    path = "/Users/user/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    path = "/home/evaldo/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
   ["nvim-tree.lua"] = {
     loaded = true,
-    path = "/Users/user/.local/share/nvim/site/pack/packer/start/nvim-tree.lua",
+    path = "/home/evaldo/.local/share/nvim/site/pack/packer/start/nvim-tree.lua",
     url = "https://github.com/kyazdani42/nvim-tree.lua"
   },
   ["nvim-treesitter"] = {
     loaded = true,
-    path = "/Users/user/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    path = "/home/evaldo/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["nvim-ts-rainbow"] = {
     loaded = true,
-    path = "/Users/user/.local/share/nvim/site/pack/packer/start/nvim-ts-rainbow",
+    path = "/home/evaldo/.local/share/nvim/site/pack/packer/start/nvim-ts-rainbow",
     url = "https://github.com/p00f/nvim-ts-rainbow"
   },
   ["nvim-web-devicons"] = {
     loaded = true,
-    path = "/Users/user/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
+    path = "/home/evaldo/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
     url = "https://github.com/kyazdani42/nvim-web-devicons"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "/Users/user/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    path = "/home/evaldo/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/Users/user/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    path = "/home/evaldo/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
   ["popup.nvim"] = {
     loaded = true,
-    path = "/Users/user/.local/share/nvim/site/pack/packer/start/popup.nvim",
+    path = "/home/evaldo/.local/share/nvim/site/pack/packer/start/popup.nvim",
     url = "https://github.com/nvim-lua/popup.nvim"
   },
   ["telescope.nvim"] = {
     loaded = true,
-    path = "/Users/user/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    path = "/home/evaldo/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
   ["tokyonight.nvim"] = {
     loaded = true,
-    path = "/Users/user/.local/share/nvim/site/pack/packer/start/tokyonight.nvim",
+    path = "/home/evaldo/.local/share/nvim/site/pack/packer/start/tokyonight.nvim",
     url = "https://github.com/folke/tokyonight.nvim"
   },
   ["vim-bbye"] = {
     loaded = true,
-    path = "/Users/user/.local/share/nvim/site/pack/packer/start/vim-bbye",
+    path = "/home/evaldo/.local/share/nvim/site/pack/packer/start/vim-bbye",
     url = "https://github.com/moll/vim-bbye"
   },
   ["vim-test"] = {
     loaded = true,
-    path = "/Users/user/.local/share/nvim/site/pack/packer/start/vim-test",
+    path = "/home/evaldo/.local/share/nvim/site/pack/packer/start/vim-test",
     url = "https://github.com/vim-test/vim-test"
   },
   ["vscode.nvim"] = {
     loaded = true,
-    path = "/Users/user/.local/share/nvim/site/pack/packer/start/vscode.nvim",
+    path = "/home/evaldo/.local/share/nvim/site/pack/packer/start/vscode.nvim",
     url = "https://github.com/Mofiqul/vscode.nvim"
   }
 }
@@ -197,5 +197,6 @@ if should_profile then save_profiles() end
 end)
 
 if not no_errors then
+  error_msg = error_msg:gsub('"', '\\"')
   vim.api.nvim_command('echohl ErrorMsg | echom "Error in packer_compiled: '..error_msg..'" | echom "Please check your config for correctness" | echohl None')
 end
