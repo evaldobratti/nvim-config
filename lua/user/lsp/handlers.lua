4ac64d7b6182ae0f743731dc317004d8ef74397c
Evaldo Bratti <evaldo.bratti@alphasights.com>
2023-05-26 11:48:09 -0300

    skip formatting

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -47,18 +47,18 @@ end
 
 local function lsp_highlight_document(client)
   -- Set autocommands conditional on server_capabilities
-  if client.server_capabilities.documentFormattingProvider then
-    vim.api.nvim_exec(
-      [[
-      augroup lsp_document_highlight
-        autocmd! * <buffer>
-        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-      augroup END
-    ]] ,
-      false
-    )
-  end
+  -- if client.server_capabilities.documentFormattingProvider then
+  --   vim.api.nvim_exec(
+  --     [[
+  --     augroup lsp_document_highlight
+  --       autocmd! * <buffer>
+  --       autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+  --       autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+  --     augroup END
+  --   ]] ,
+  --     false
+  --   )
+  -- end
 end
 
 local function lsp_keymaps(bufnr)
